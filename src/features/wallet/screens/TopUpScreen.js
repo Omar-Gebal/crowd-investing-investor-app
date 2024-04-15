42db45c9caf9183bc0ae411f22f5e51c0c7e56e0
Omar Emad <2001omaremad@gmail.com>
2024-04-15 16:38:04 +0200

    some ui adjustments

---
+++ src/features/wallet/screens/TopUpScreen.js
@@ -0,0 +1,17 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+function TopUpScreen(props) {
+    return (
+        <CustomSafeArea>
+
+        </CustomSafeArea>
+    );
+
+}
+
+const styles = StyleSheet.create({
+ 
+});
+
+
+export default TopUpScreen;
